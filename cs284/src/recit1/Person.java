21dcc36902ba11ebb9366395294ea1d585a0392e
smax253 <smax253@gmail.com>
2020-01-24 17:06:00 -0500

    code written on 1/24/2020

---
+++ cs284/src/recit1/Person.java
@@ -0,0 +1,9 @@
+package recit1;
+
+public interface Person {
+	public void doJob();
+	public String getName();
+	public int getAge();
+	public String getAddress();
+	public void setAddress(String address);
+}
